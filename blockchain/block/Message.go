ddc982cd114648e20af609be6abfef763364aceb
Clement Teyssandier <c.teyssa@gmail.com>
2019-04-03 16:37:59 +0200

    Début mise en forme

---
+++ blockchain/block/Message.go
@@ -0,0 +1,6 @@
+package block
+
+// Message is ised for writing block informations
+type Message struct {
+	BPM int
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	. "./blockchain"
+)
+
+func main() {
+	launchaaa()
+}
